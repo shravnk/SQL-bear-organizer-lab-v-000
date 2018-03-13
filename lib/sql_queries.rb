176391cbdd353b35061a0ba8700b4a26cb4b06c9
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-13 15:45:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,12 @@ end
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(*) AS FREQUENCY
   FROM bears
-  GROUP BY color DESC LIMIT 1;"
+  GROUP BY color
+  HAVING COUNT(*) >= ALL(
+  SELECT COUNT(*)
+  FROM bears
+  GROUP BY color
+  );"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
